54cce9ffe4acdaac3edffafdbeae486305993d4d
NEHA MALVIA <46103180+nehasm@users.noreply.github.com>
2020-06-16 03:18:17 +0530

    Tree

---
+++ Tree/TreeLevelOrderTraversal.py
@@ -0,0 +1,73 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+def levelOrder(root):
+    if not root:
+        return
+    queue=[]
+    level_nodes=[]
+    queue.append(root)
+    while(len(queue)):
+        root=queue[0]
+        #take node for printing
+        level_nodes.append(root.info)
+        #take left and right node in queue
+        if root.left:
+            queue.append(root.left)
+        if root.right:
+            queue.append(root.right)
+        #dequeue the node
+        queue.pop(0)
+    for i in level_nodes: 
+        print(i, end = " ")
+
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+levelOrder(tree.root)
